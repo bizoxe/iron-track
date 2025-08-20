77192a7f5c4e5e48aaf2bc94b3c5e69fbe10d089
alexander <alex.matveev.dev@gmail.com>
2025-08-20 16:07:39 +0300

    add(lib): a custom  class for json response

---
+++ fastapi-app/library/json_response.py
@@ -0,0 +1,28 @@
+from typing import (
+    Any,
+    Mapping,
+)
+
+import msgspec
+from fastapi import Response
+from fastapi.encoders import jsonable_encoder
+from starlette.background import BackgroundTask
+
+
+class MsgSpecJSONResponse(Response):
+    """
+    JSON response using the high-performance `msgspec` library to serialize data to JSON.
+    """
+
+    def __init__(
+        self,
+        content: Any,
+        status_code: int = 200,
+        headers: Mapping[str, str] | None = None,
+        media_type: str = "application/json",
+        background: BackgroundTask | None = None,
+    ) -> None:
+        super().__init__(content, status_code, headers, media_type, background)
+
+    def render(self, content: Any) -> bytes:
+        return msgspec.json.encode(jsonable_encoder(content))
