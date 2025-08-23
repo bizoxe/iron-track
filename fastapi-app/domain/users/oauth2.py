6ab5c554e2bdda71a49a3b7628f574ce3e23a15c
alexander <alex.matveev.dev@gmail.com>
2025-08-23 19:30:01 +0300

    add Authenticate helper class for user authentication

---
+++ fastapi-app/domain/users/oauth2.py
@@ -0,0 +1,104 @@
+import logging
+from collections.abc import (
+    Awaitable,
+    Callable,
+)
+from typing import (
+    Annotated,
+    Any,
+)
+
+from advanced_alchemy.exceptions import NotFoundError
+from fastapi import Depends
+from jwt import PyJWTError
+
+from domain.users.deps import UserService
+from domain.users.schemas import UserAuth
+from library.exceptions import (
+    PermissionDeniedException,
+    UnauthorizedException,
+    UserNotFound,
+)
+from library.jwt_utils import decode_jwt
+from library.oauth2 import (
+    access_token,
+    refresh_token,
+)
+
+__all__ = ("Authenticate",)
+
+log = logging.getLogger(__name__)
+
+
+def get_payload_from_token(authentication_token: str) -> dict[str, Any] | None:
+    try:
+        payload = decode_jwt(token=authentication_token)
+        return payload
+    except PyJWTError:
+        log.exception(
+            "An error occurred when decoding the token",
+            exc_info=True,
+        )
+
+    return None
+
+
+def get_user_by_token_sub(pyload: dict[str, Any] | None) -> str:
+    if pyload is not None:
+        user_id: str = pyload["sub"]
+        return user_id
+    raise UnauthorizedException(message="Invalid or expired token")
+
+
+class Authenticate:
+    @classmethod
+    async def get_current_user_for_refresh(
+        cls,
+        token: Annotated[str, Depends(refresh_token)],
+        users_service: UserService,
+    ) -> UserAuth:
+        token_payload = get_payload_from_token(authentication_token=token)
+        user_id = get_user_by_token_sub(pyload=token_payload)
+        try:
+            db_obj = await users_service.get(user_id)
+            if not db_obj.is_active:
+                raise UnauthorizedException(message="Inactive user")
+            return users_service.to_schema(db_obj, schema_type=UserAuth)
+        except NotFoundError:
+            raise UserNotFound(user_id=user_id)
+
+    @classmethod
+    async def get_current_user(
+        cls,
+        token: Annotated[str, Depends(access_token)],
+        users_service: UserService,
+    ) -> UserAuth:
+        token_payload = get_payload_from_token(authentication_token=token)
+        user_id = get_user_by_token_sub(pyload=token_payload)
+        try:
+            db_obj = await users_service.get(user_id)
+            return users_service.to_schema(db_obj, schema_type=UserAuth)
+        except NotFoundError:
+            raise UserNotFound(user_id=user_id)
+
+    @classmethod
+    def get_current_active_user(cls) -> Callable[[UserAuth], Awaitable[UserAuth]]:
+        async def current_user(
+            user_auth: Annotated[UserAuth, Depends(cls.get_current_user)],
+        ) -> UserAuth:
+            if not user_auth.is_active:
+                raise UnauthorizedException(message="Inactive user")
+            return user_auth
+
+        return current_user
+
+    @classmethod
+    def superuser_required(cls) -> Callable[[UserAuth], Awaitable[UserAuth]]:
+        async def current_user(
+            user_auth: Annotated[UserAuth, Depends(cls.get_current_user)],
+        ) -> UserAuth:
+            if not user_auth.is_superuser:
+                raise PermissionDeniedException(message="You do not have sufficient rights to access the resource")
+            return user_auth
+
+        return current_user
