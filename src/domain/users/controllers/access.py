f13e46c5d2af5fabecf25cfefe7437a33b1f57a0
alexander <alex.matveev.dev@gmail.com>
2025-11-03 18:37:39 +0300

    feat(users): Implement full authentication and access management API

---
+++ src/domain/users/controllers/access.py
@@ -0,0 +1,218 @@
+from typing import Annotated
+
+from advanced_alchemy.exceptions import DuplicateKeyError
+from fastapi import (
+    APIRouter,
+    Depends,
+    Response,
+    status,
+)
+from fastapi.security import OAuth2PasswordRequestForm
+from starlette.background import (
+    BackgroundTask,
+    BackgroundTasks,
+)
+
+from src.config import constants
+from src.config.base import get_settings
+from src.domain.users import urls
+from src.domain.users.auth import Authenticate
+from src.domain.users.deps import (
+    RoleServiceDep,
+    UserServiceDep,
+)
+from src.domain.users.jwt_helpers import (
+    add_token_to_blacklist,
+    create_access_token,
+    create_refresh_token,
+)
+from src.domain.users.schemas import (
+    AccountRegister,
+    PasswordUpdate,
+    User,
+    UserAuth,
+)
+from src.domain.users.utils import perform_logout_cleanup
+from src.lib.deps import RedisClientDep
+from src.lib.exceptions import (
+    ConflictException,
+)
+from src.lib.invalidate_cache import invalidate_user_cache
+from src.lib.json_response import MsgSpecJSONResponse
+
+access_router = APIRouter(
+    tags=["Access"],
+    default_response_class=MsgSpecJSONResponse,
+)
+
+settings = get_settings()
+
+
+@access_router.post(
+    path=urls.ACCOUNT_REGISTER,
+    status_code=status.HTTP_201_CREATED,
+    operation_id="AccountRegister",
+    name="access:signup",
+    summary="Register a new user.",
+)
+async def signup(
+    users_service: UserServiceDep,
+    roles_service: RoleServiceDep,
+    account_register: AccountRegister,
+) -> User:
+    """User Signup."""
+    role_obj = await roles_service.get_default_role(
+        default_role_slug=users_service.default_role,
+    )
+    try:
+        user = await users_service.create(
+            data=account_register.model_dump() | {"role_id": role_obj.id},
+        )
+        return users_service.to_schema(user, schema_type=User)
+    except DuplicateKeyError as exc:
+        msg = "A user with this email already exists"
+        raise ConflictException(message=msg) from exc
+
+
+@access_router.post(
+    path=urls.ACCOUNT_LOGIN,
+    operation_id="AccountLogin",
+    status_code=status.HTTP_204_NO_CONTENT,
+    name="access:login",
+    summary="Account login, issue access and refresh tokens.",
+)
+async def login_for_access_token(
+    users_service: UserServiceDep,
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+) -> Response:
+    """Issue access and refresh tokens. Store tokens in cookies."""
+    user = await users_service.authenticate(
+        username=form_data.username,
+        password=form_data.password,
+    )
+    response = Response(status_code=status.HTTP_204_NO_CONTENT)
+    access_token = create_access_token(user_id=user.id, email=user.email)
+    refresh_token = create_refresh_token(user_id=user.id)
+    response.set_cookie(
+        key="access_token",
+        value=access_token,
+        max_age=constants.ACCESS_TOKEN_MAX_AGE,
+        httponly=True,
+        samesite="lax",
+        secure=constants.COOKIE_SECURE_VALUE,
+    )
+    response.set_cookie(
+        key="refresh_token",
+        value=refresh_token,
+        max_age=constants.REFRESH_TOKEN_MAX_AGE,
+        httponly=True,
+        samesite="strict",
+        secure=constants.COOKIE_SECURE_VALUE,
+    )
+
+    return response
+
+
+@access_router.post(
+    path=urls.ACCOUNT_REFRESH_TOKEN,
+    operation_id="RefreshAccessToken",
+    status_code=status.HTTP_204_NO_CONTENT,
+    name="access:refresh",
+    summary="Issue a new access token using the refresh token.",
+)
+async def user_auth_refresh_token(
+    user_auth: Annotated[UserAuth, Depends(Authenticate.get_current_user_for_refresh)],
+    redis_client: RedisClientDep,
+) -> Response:
+    """Get the user by the refresh token and issue a new access token."""
+    access_token = create_access_token(
+        user_id=user_auth.id,
+        email=user_auth.email,
+    )
+    refresh_token = create_refresh_token(user_id=user_auth.id)
+    background_task = BackgroundTask(
+        func=add_token_to_blacklist,
+        refresh_token_identifier=user_auth._refresh_jti,  # type: ignore[arg-type]  # noqa: SLF001
+        redis_client=redis_client,
+    )
+    response = Response(status_code=status.HTTP_204_NO_CONTENT, background=background_task)
+    response.set_cookie(
+        key="access_token",
+        value=access_token,
+        max_age=constants.ACCESS_TOKEN_MAX_AGE,
+        httponly=True,
+        samesite="lax",
+        secure=constants.COOKIE_SECURE_VALUE,
+    )
+    response.set_cookie(
+        key="refresh_token",
+        value=refresh_token,
+        max_age=constants.REFRESH_TOKEN_MAX_AGE,
+        httponly=True,
+        samesite="strict",
+        secure=constants.COOKIE_SECURE_VALUE,
+    )
+
+    return response
+
+
+@access_router.post(
+    path=urls.ACCOUNT_LOGOUT,
+    operation_id="AccountLogout",
+    name="access:logout",
+    summary="Log out, delete tokens from cookie.",
+)
+async def logout(
+    user_auth: Annotated[UserAuth, Depends(Authenticate.get_current_active_user())],
+    refresh_jti: Annotated[str, Depends(Authenticate.get_refresh_jti)],
+    redis_client: RedisClientDep,
+) -> Response:
+    """User Logout."""
+    background_tasks = BackgroundTasks()
+    background_tasks.add_task(
+        func=perform_logout_cleanup,
+        refresh_jti=refresh_jti,
+        user_id=user_auth.id,
+        redis_client=redis_client,
+    )
+    response = Response(
+        status_code=status.HTTP_204_NO_CONTENT,
+        background=background_tasks,
+    )
+    response.delete_cookie("access_token")
+    response.delete_cookie("refresh_token")
+
+    return response
+
+
+@access_router.patch(
+    path=urls.ACCOUNT_PWD_UPDATE,
+    operation_id="AccountUpdatePwd",
+    name="access:update-pwd",
+    summary="Update your user password.",
+)
+async def update_password(
+    user_auth: Annotated[UserAuth, Depends(Authenticate.get_current_active_user())],
+    users_service: UserServiceDep,
+    pwd_data: PasswordUpdate,
+) -> Response:
+    """Update user password."""
+    await users_service.update_password(
+        data=pwd_data,
+        user_id=user_auth.id,
+    )
+    await invalidate_user_cache(user_id=user_auth.id)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@access_router.get(
+    path=urls.ACCOUNT_PROFILE,
+    operation_id="AccountProfile",
+    name="access:profile",
+    summary="Get information about yourself by the user.",
+)
+async def user_auth_get_self_info(
+    user_auth: Annotated[UserAuth, Depends(Authenticate.get_current_active_user())],
+) -> UserAuth:
+    """Get self account info."""
+    return user_auth
